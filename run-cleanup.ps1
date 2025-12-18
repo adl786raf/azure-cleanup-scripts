0a89c3d109d98847c9f88bd64df91bf1dbeee58f
adl786raf <adl786raf@gmail.com>
2025-12-18 15:31:43 +0000

    Add wrapper script to auto-pull and run cleanup

---
+++ run-cleanup.ps1
@@ -0,0 +1,8 @@
+# Navigate to repo folder
+cd C:\Users\Administrator\azure-cleanup-scripts
+
+# Pull latest changes from GitHub
+git pull origin main
+
+# Run the cleanup script
+.\cleanup-azure.ps1
